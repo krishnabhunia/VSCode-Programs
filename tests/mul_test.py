9030d25b1603d9bb7b24a14d3660ea3903d1039a
KRISHNA DIPAYAN BHUNIA <kri.subsc+github@gmail.com>
2023-10-01 23:32:43 +0530

    test name main

---
+++ tests/mul_test.py
@@ -0,0 +1,22 @@
+import multiprocessing as mp
+import time
+def test_che(res = 0):
+    print("Test start with res = ", res)
+    time.sleep(res)
+    print("Test stop")
+
+def check():
+    try:
+        proc = mp.Process(target=test_che,args=([10]))
+        proc.start()
+        proc.join(timeout=3)
+        if proc.is_alive():
+            print("process is alive")
+            proc.terminate()
+        else:
+            print("process ended normally")
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    check()
